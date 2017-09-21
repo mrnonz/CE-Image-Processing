a730dcf074d9c1c9722f1488a4ffa243cce4d12b
mrnonz <nontawat39@gmail.com>
2017-09-21 11:31:06 +0700

    Style code :tea:

--- Lab2/sketch.py
+++ Lab2/sketch.py
@@ -11,13 +11,17 @@ scharr = np.array([[-3 - 3j, 0 - 10j,  +3 - 3j],
                    [-3 + 3j, 0 + 10j,  +3 + 3j]])  # Gx + j*Gy
 grad = signal.convolve2d(lena, scharr, boundary='symm', mode='same')
 cv2.imshow("test", np.absolute(grad))
+
 fig, (ax_orig, ax_mag, ax_ang) = plt.subplots(1, 3)
+
 ax_orig.imshow(lena, cmap='gray')
 ax_orig.set_title('Original')
 ax_orig.set_axis_off()
+
 ax_mag.imshow(np.absolute(grad), cmap='gray')
 ax_mag.set_title('Gradient magnitude')
 ax_mag.set_axis_off()
+
 ax_ang.imshow(np.angle(grad), cmap='hsv')  # hsv is cyclic, like angles
 ax_ang.set_title('Gradient orientation')
 ax_ang.set_axis_off()
